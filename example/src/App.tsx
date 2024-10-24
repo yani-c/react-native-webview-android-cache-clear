7656475667f8dd5e77a9749ccfdedfbead352650
yani-c <yaniceli1d@gmail.com>
2024-10-24 11:48:16 -0300

    add react native webview to example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,26 +1,41 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useRef, useState } from 'react';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import ReactNativeWebviewAndroidCacheClear from 'react-native-webview-android-cache-clear';
+import WebView from 'react-native-webview';
 
 export default function App() {
-  const [result] = useState();
+  const [result, setResult] = useState();
+  const webviewRef = useRef(null); // Create a ref for the WebView
 
   const test = async () => {
     try {
       const res = await ReactNativeWebviewAndroidCacheClear.clearCache();
       console.log('sucess!!!', res); // Handle the success response
+      setResult(res);
     } catch (error) {
       console.error('Error clearing cache:', error); // Handle the error
     }
   };
 
-  useEffect(() => {
-    test();
-  }, []); // Pass an empty array to run only once on mount
+  const reloadWebView = () => {
+    if (webviewRef.current) {
+      webviewRef.current.reload(); // Reload the WebView
+    }
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text> Cache cleared result: {result}</Text>
+      <View style={styles.buttons}>
+        <Button title="Clear Cache" onPress={test} />
+        <Button title="Reload WebView" onPress={reloadWebView} />
+      </View>
+      <WebView
+        startInLoadingState
+        source={{ uri: 'https://stage-dashboard.powerme.health' }}
+        originWhitelist={['*']}
+        containerStyle={styles.webview}
+      />
     </View>
   );
 }
@@ -30,10 +45,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    marginVertical: 15,
   },
-  box: {
-    width: 60,
-    height: 60,
+  webview: {
+    display: 'flex',
+    width: '100%',
+  },
+  buttons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
     marginVertical: 20,
   },
 });
