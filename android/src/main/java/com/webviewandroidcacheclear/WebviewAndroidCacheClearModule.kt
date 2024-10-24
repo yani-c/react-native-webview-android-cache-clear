652de3df56618fb1b92191824836b994df5c6358
yani-c <yaniceli1d@gmail.com>
2024-10-24 11:00:24 -0300

    fix webview uninitialized

--- android/src/main/java/com/webviewandroidcacheclear/WebviewAndroidCacheClearModule.kt
+++ android/src/main/java/com/webviewandroidcacheclear/WebviewAndroidCacheClearModule.kt
@@ -1,39 +1,49 @@
 package com.webviewandroidcacheclear
 
+import android.webkit.WebView
 import android.webkit.CookieManager
 import android.webkit.WebStorage
+
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
 class WebviewAndroidCacheClearModule(reactContext: ReactApplicationContext) :
-    ReactContextBaseJavaModule(reactContext) {
+  ReactContextBaseJavaModule(reactContext) {
 
-    override fun getName(): String {
-        return NAME
-    }
+  private var webView: WebView? = null
 
-    @ReactMethod
-    fun clearCache(promise: Promise) {
-        try {
-            // Clear all WebView instances cache
-            WebStorage.getInstance().deleteAllData()
-            // Clear all the cookies
-            CookieManager.getInstance().removeAllCookies { success ->
-                if (success) {
-                    promise.resolve("Cache cleared")
-                } else {
-                    promise.reject("CACHE_CLEAR_ERROR", "Failed to clear cookies")
-                }
-            }
-            CookieManager.getInstance().flush()
-        } catch (e: Exception) {
-            promise.reject("CACHE_CLEAR_ERROR", "Failed to clear cache", e)
-        }
-    }
+  override fun getName(): String {
+    return NAME
+  }
 
-    companion object {
-        const val NAME = "WebviewAndroidCacheClear"
-    }
+  fun setWebView(webView: WebView) {
+    this.webView = webView
+  }
+
+  @ReactMethod
+  fun clearCache(promise: Promise) {
+      WebStorage.getInstance().deleteAllData();
+      CookieManager.getInstance().removeAllCookies(null);
+      CookieManager.getInstance().flush();
+
+      // Get the current activity from the React context
+      val currentActivity = getCurrentActivity()
+      currentActivity?.runOnUiThread {
+          try {
+            val webView = WebView(currentActivity)
+            webView.clearCache(true)
+            promise.resolve("Cache cleared")
+          } catch (e: Exception) {
+              promise.reject("CACHE_CLEAR_ERROR", "Failed to clear cache", e)
+          }
+      } ?: run {
+          promise.reject("CACHE_CLEAR_ERROR", "Current activity is null")
+      }
+  }
+
+  companion object {
+    const val NAME = "WebviewAndroidCacheClear"
+  }
 }
